efe1c6c1c0307f59f0ee588a060d7fad4e0a1efa
PiotrRomanczuk <p.romanczuk@gmail.com>
2023-07-11 19:14:31 +0200

    Init basic server with REST tests

--- server.js
+++ server.js
@@ -6,25 +6,17 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-	res.sendStatus(200);
-	console.log('Get method works');
-});
-
-app.post('/', (req, res) => {
-	console.log(req.body);
-	console.log('Hi from beginning of the POST process');
-	const { name, location } = req.body;
-	if (name != undefined && location != undefined) {
-		res.status(200).send({
-			message: `YOUR KEYS WERE ${name}, ${location}`,
-		});
-	} else {
-		console.log(`Error`);
+app.get('/', async (req, res) => {
+	try {
+		const data = await pool.query('SELECT * FROM schools');
+		res.status(200).send(data.rows);
+	} catch (err) {
+		console.log(err);
+		res.sendStatus(500);
 	}
 });
 
-app.post('/user', async (req, res) => {
+app.post('/', async (req, res) => {
 	const { name, location } = req.body;
 	try {
 		await pool.query('INSERT INTO schools (name, address) VALUES ($1, $2)', [
@@ -38,8 +30,24 @@ app.post('/user', async (req, res) => {
 	}
 });
 
-app.get('setup', (req, res) => {
+app.post('/test', (req, res) => {
+	console.log(req.body);
+	console.log('Hi from beginning of the POST process');
+	const { name, location } = req.body;
+	if (name != undefined && location != undefined) {
+		res.status(200).send({
+			message: `YOUR KEYS WERE ${name}, ${location}`,
+		});
+	} else {
+		console.log(`Error`);
+	}
+});
+
+app.get('/setup', async (req, res) => {
 	try {
+		await pool.query(
+			'CREATE TABLE schools ( id SERIAL PRIMARY KEY, name VARCHAR(100) NOT NULL, address VARCHAR(100) NOT NULL)'
+		);
 	} catch (error) {
 		res.sendStatus(500);
 		console.log(`Error: ${error}`);
